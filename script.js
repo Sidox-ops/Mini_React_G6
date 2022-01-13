7b5fe419e1b48bcfbd3bdfda41a73ba3cc028c87
NilsMillot <compte.mxs@hotmail.fr>
2022-01-13 08:32:43 +0100

    create mini react logic

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
 const root = document.querySelector('#root');
 
+function renderReactElement(element) {
+    const domElement = element.render();
+    domElement.props = {
+        key: element?.props?.key,
+        ...domElement?.props
+    };
+
+    return domElement;
+}
+
+const MiniReact = {
+Component: class Component {},
+
+createElement(type, attributes = null, children){
+    if (typeof type === 'function') {
+    const reactElement = new (type)(attributes);
+    reactElement.children = children;
+
+    return renderReactElement(reactElement);
+    }
+    
+    return {
+    type: type,
+    attributes: attributes,
+    children: children,
+    };
+}
+};
+
+class Hello extends MiniReact.Component {
+    static propTypes = {
+      properties: {
+        toWhat: {
+          type: "string",
+        },
+      },
+    };
+    render() {
+      return MiniReact.createElement("div", null, ["Hello wordl"]);
+    }
+  }
+
+class App extends MiniReact.Component {
+    state = {
+        users: [1, 2, 3],
+    };
+    constructor(){
+        super();
+        // this.state.currentUser = this.props.currentUser;
+    }
+    render() {
+        // return MiniReact.createElement("div", null, ["coucou"])
+        return MiniReact.createElement(Hello, null, [
+        MiniReact.createElement("h1", null, ["Hello x2"])
+        ])
+    }
+}
 function link(label,path){
     const a = document.createElement('a');
     const textA = document.createTextNode(label);
@@ -60,30 +117,70 @@ function Page3() {
     return h1;
 }
 
-function generatePage(){
-
+String.prototype.interpolate = function(attributes) {
+    return this;
+  }
+  
+function generatePage() {
+    // document.title = history.state.title;
     const currentPath = window.location.pathname;
     let elem;
-    switch(currentPath){
-        case '/page1':
-            elem = Page1();
-            break;
-        case '/page2':
-            elem = Page2();
-            break;
-        case '/page3':
-            elem = Page3();
-            break;
+    switch (currentPath) {
+      case "/page1":
+        elem = Page1();
+        break;
+      case "/page2":
+        elem = Page2();
+        break;
+      case "/page3":
+        let app = new App();
+        elem = app.render();
+        break;
     }
-    if(root.childNodes.length){
-        root.replaceChild(elem,root.firstChild)
-    }else{
-        root.appendChild(elem);// Appeler toujours l'element du DOM en dernier pour le painting
+    if (root.firstChild) {
+      root.replaceChild(generateStructure(elem), root.firstChild);
+    } else {
+      root.appendChild(generateStructure(elem));
     }
 }
 
-root.addEventListener("rerender",generatePage);
+root.addEventListener("rerender", generatePage);
 
-window.onpopstate = ()=> root.dispatchEvent(new Event('rerender'));
+window.onpopstate = () => root.dispatchEvent(new Event("rerender"));
 
-root.dispatchEvent(new Event('rerender')); // = generatePage();
+const generateStructure = (structure) => {
+    const node = document.createElement(structure.type);
+    console.log('%cscript_migration.js line:275 node', 'color: #007acc;', node);
+    if (structure.attributes) {
+      for (let attName in structure.attributes) {
+        if (/on([A-Z].*)/.test(attName)) {
+          const eventName = attName.match(/on([A-Z].*)/)[1].toLowerCase();
+          node.addEventListener(eventName, structure.attributes[attName]);
+        } else {
+          node.setAttribute(attName, structure.attributes[attName]);
+        }
+      }
+    }
+    if (structure.dataset) {
+      for (let attName in structure.dataset) {
+        node.dataset[attName] = structure.dataset[attName];
+      }
+    }
+    if (structure.children)
+      for (let child of structure.children) {
+        console.log('%cscript_migration.js line:293 child', 'color: #007acc;', child);
+        if (child === undefined) continue;
+        if (typeof child === "string") {
+          node.appendChild(
+            // document.createTextNode(child)
+            document.createTextNode(child.interpolate(structure.attributes))
+          );
+        } else {
+          node.appendChild(generateStructure(child));
+        }
+      }
+    structure.node = node;
+  
+    return node;
+};
+root.dispatchEvent(new Event("rerender"));
