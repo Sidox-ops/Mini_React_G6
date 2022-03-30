6b1c64c552f3b4ae8d84bca51ba8bd0d7d924b84
Océane Renoux <oceane.renoux@hotmail.fr>
2022-03-30 21:36:50 +0200

    [MODIFY] script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const root = document.querySelector('#root');
 root.addEventListener("rerender", generatePage);
 window.onpopstate = () => root.dispatchEvent(new Event("rerender"));
+root.dispatchEvent(new Event("rerender"));
