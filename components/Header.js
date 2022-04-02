7fa42d4b5baeb13f638371d9a1474f3bacbc2a1d
Sidox <pro.sidox@gmail.com>
2022-04-02 02:27:26 +0200

    update buttons

--- components/Header.js
+++ components/Header.js
@@ -11,9 +11,9 @@ export class Header extends MiniReact.Component {
   render() {
     return MiniReact.createElement("div", { class: "header" }, [
       MiniReact.createElement("div", { class: "menu" }, [
-        MiniReact.createElement("a", { class: "button", href:"/" }, ["bouton"]),
-        MiniReact.createElement("a", { class: "button", href:"/meteo" }, ["bouton"]),
-        MiniReact.createElement("a", { class: "button", href:"/uploader" }, ["bouton"]),
+        MiniReact.createElement("a", { class: "button", href:"/" }, ["HOME"]),
+        MiniReact.createElement("a", { class: "button", href:"/meteo" }, ["METEO"]),
+        MiniReact.createElement("a", { class: "button", href:"/uploader" }, ["UPLOADER"]),
         MiniReact.createElement("a", { class: "button" }, ["bouton"])
       ])
 
